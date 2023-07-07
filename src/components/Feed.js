d93c5d1a4b3b79dcd1e07a26cb7d232ea21aacbf
joelwaiman <waimanjoel@gmail.com>
2023-07-07 18:09:36 -0300

    ya se puede tw

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './feed.css'
 import { useState } from 'react';
 import img from '../assets/user.jpg'
+import { IoLocationOutline, IoImageOutline } from "react-icons/io5";
+import { BsFiletypeGif, BsListUl, BsEmojiSmile } from "react-icons/bs";
+
 
 const Feed = () => {
 
@@ -9,31 +12,52 @@ const Feed = () => {
     const [input, setInput] = useState('');
 
     const handlerTweets = (e) => {
-        const {value} = e.target;
+        const { value } = e.target;
         setInput(value)
     }
 
     const addTweet = () => {
-        const newTweet = ([...tweets, input])
+        const newTweet = ([ input, ...tweets])
         setInput('')
         setTweets(newTweet)
     }
 
-    return(
+    const icons = [
+        <IoImageOutline />,
+        <BsFiletypeGif />,
+        <BsListUl />,
+        <BsEmojiSmile />,
+        <IoLocationOutline />,
+    ]
+
+    return (
         <div className='container-feed'>
-            <p>Inicio</p>
+            <p className='title'>Inicio</p>
             <div className='container-input'>
-                <img className='user-img' src={img} alt='egg' />
-                <input className='input' onChange={handlerTweets} value={input} placeholder='Que estas pensando?!'/>
-                <button disabled={input===''} onClick={addTweet}>Twittear</button>
+                <div className='container-input-tweet'>
+                    <img className='user-img' src={img} alt='egg' />
+                    <input className='input' onChange={handlerTweets} value={input} placeholder='¡¿Qué está pasando?!' />
+                </div>
+                <div className='container-input-button'>
+                    <div className='icon'>
+                        {icons.map((icon) => {
+                            return <p>{icon}</p>
+                        })}
+                    </div>
+                    <button disabled={input === ''} onClick={addTweet}>Twittear</button>
+                </div>
             </div>
-            <ul className='container-tweets'>
-                <li>
-                    {tweets.map((data)=>{
-                        return <p key={data}>{data}</p>
-                    })}
-                </li>
-            </ul>
+                {tweets.map((data) => {
+                    return <div className='container-tweets'>
+                        <div>
+                            <img src={img} alt='egg' />
+                        </div>
+                        <div className='container-tweets-data'>
+                            <p><b>User </b> <span> @egg</span></p>
+                            <p className='tweet'>{data}</p>
+                        </div>
+                    </div>
+                })}
         </div>
     )
 }
